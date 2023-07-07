f2ede4f2c267f75f7f897a6033443bb38b20abb5
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-07 10:02:43 +0900

    Update platform_tools.py

--- android_sdk_supporter/platform_tools.py
+++ android_sdk_supporter/platform_tools.py
@@ -10,6 +10,7 @@ class PlatformTools:
         self.android_sdk_directory = android_sdk_directory
         self.devices = []
 
+        #platform-tools
         if platform.system() == 'Darwin': #맥
             from_zip = f"{self.android_sdk_directory}/platform-tools_r34.0.3-darwin.zip"
             if not os.path.exists(from_zip):
@@ -28,7 +29,47 @@ class PlatformTools:
                 zip_file = zipfile.ZipFile(from_zip)
                 zip_file.extractall(self.android_sdk_directory)
                 zip_file.close()
-        
+
+        #build-tools
+        if platform.system() == 'Darwin': #맥
+            from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-macosx.zip"
+            if not os.path.exists(from_zip):
+                zip_file = zipfile.ZipFile(from_zip)
+                zip_file.extractall(self.android_sdk_directory)
+                zip_file.close()
+        elif platform.system() == 'Windows': #윈도우
+            from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-windows.zip"
+            if not os.path.exists(from_zip):
+                zip_file = zipfile.ZipFile(from_zip)
+                zip_file.extractall(self.android_sdk_directory)
+                zip_file.close()
+        elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+            from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-linux.zip"
+            if not os.path.exists(from_zip):
+                zip_file = zipfile.ZipFile(from_zip)
+                zip_file.extractall(self.android_sdk_directory)
+                zip_file.close()
+
+        #cmdline-tools
+        if platform.system() == 'Darwin': #맥
+            from_zip = f"{self.android_sdk_directory}/cmdline-tools-mac-9477386_latest.zip"
+            if not os.path.exists(from_zip):
+                zip_file = zipfile.ZipFile(from_zip)
+                zip_file.extractall(self.android_sdk_directory)
+                zip_file.close()
+        elif platform.system() == 'Windows': #윈도우
+            from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-windows.zip"
+            if not os.path.exists(from_zip):
+                zip_file = zipfile.ZipFile(from_zip)
+                zip_file.extractall(self.android_sdk_directory)
+                zip_file.close()
+        elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+            from_zip = f"{self.android_sdk_directory}/cmdline-tools-linux-9477386_latest.zip"
+            if not os.path.exists(from_zip):
+                zip_file = zipfile.ZipFile(from_zip)
+                zip_file.extractall(self.android_sdk_directory)
+                zip_file.close()
+    
     '''
 $ adb devices
 List of devices attached
