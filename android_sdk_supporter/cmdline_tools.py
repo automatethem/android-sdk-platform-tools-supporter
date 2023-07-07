f95c54b68ed683389a50940b425f5bf50e81d397
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-07 11:32:22 +0900

    Update cmdline_tools.py

--- android_sdk_supporter/cmdline_tools.py
+++ android_sdk_supporter/cmdline_tools.py
@@ -10,22 +10,23 @@ class CmdlineTools:
         super().__init__()
         self.android_sdk_directory = android_sdk_directory
 
-        #cmdline-tools
-        if platform.system() == 'Darwin': #맥
-            from_zip = f"{self.android_sdk_directory}/commandlinetools-mac-9477386_latest.zip"
-            if not os.path.exists(from_zip):
-                zip_file = zipfile.ZipFile(from_zip)
-                zip_file.extractall(self.android_sdk_directory)
-                zip_file.close()
-        elif platform.system() == 'Windows': #윈도우
-            from_zip = f"{self.android_sdk_directory}/commandlinetools-win-9477386_latest.zip"
-            if not os.path.exists(from_zip):
-                zip_file = zipfile.ZipFile(from_zip)
-                zip_file.extractall(self.android_sdk_directory)
-                zip_file.close()
-        elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
-            from_zip = f"{self.android_sdk_directory}/commandlinetools-linux-9477386_latest.zip"
-            if not os.path.exists(from_zip):
-                zip_file = zipfile.ZipFile(from_zip)
-                zip_file.extractall(self.android_sdk_directory)
-                zip_file.close()
+        if self.android_sdk_directory:
+            #cmdline-tools
+            cmdline_tools_directory = f"{self.android_sdk_directory}/cmdline-tools"
+            if not os.path.exists(cmdline_tools_directory):
+                if platform.system() == 'Darwin': #맥
+                    from_zip = f"{self.android_sdk_directory}/commandlinetools-mac-9477386_latest.zip"
+                    zip_file = zipfile.ZipFile(from_zip)
+                    zip_file.extractall(self.android_sdk_directory)
+                    zip_file.close()
+                elif platform.system() == 'Windows': #윈도우
+                    from_zip = f"{self.android_sdk_directory}/commandlinetools-win-9477386_latest.zip"
+                    zip_file = zipfile.ZipFile(from_zip)
+                    zip_file.extractall(self.android_sdk_directory)
+                    zip_file.close()
+                elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+                    from_zip = f"{self.android_sdk_directory}/commandlinetools-linux-9477386_latest.zip"
+                    zip_file = zipfile.ZipFile(from_zip)
+                    zip_file.extractall(self.android_sdk_directory)
+                    zip_file.close()
+    
