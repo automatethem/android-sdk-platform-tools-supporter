2cf98f34124bea4c3d8e6e8464132472740913be
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-07 10:14:39 +0900

    Update platform_tools.py

--- android_sdk_supporter/platform_tools.py
+++ android_sdk_supporter/platform_tools.py
@@ -3,6 +3,7 @@ import platform
 import subprocess
 import zipfile
 import sys
+import shutil
 
 class PlatformTools:
     def __init__(self, android_sdk_directory=None):
@@ -37,19 +38,21 @@ class PlatformTools:
                 zip_file = zipfile.ZipFile(from_zip)
                 zip_file.extractall(self.android_sdk_directory)
                 zip_file.close()
-                os.mv(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
+                shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
         elif platform.system() == 'Windows': #윈도우
             from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-windows.zip"
             if not os.path.exists(from_zip):
                 zip_file = zipfile.ZipFile(from_zip)
                 zip_file.extractall(self.android_sdk_directory)
                 zip_file.close()
+                shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
         elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
             from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-linux.zip"
             if not os.path.exists(from_zip):
                 zip_file = zipfile.ZipFile(from_zip)
                 zip_file.extractall(self.android_sdk_directory)
                 zip_file.close()
+                shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
 
         #cmdline-tools
         if platform.system() == 'Darwin': #맥
