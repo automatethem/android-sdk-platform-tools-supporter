5aab19f61bfb4977a67eb56b408f32c50b7b7ff0
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-07 11:31:14 +0900

    Update build_tools.py

--- android_sdk_supporter/build_tools.py
+++ android_sdk_supporter/build_tools.py
@@ -10,25 +10,26 @@ class BuildTools:
         super().__init__()
         self.android_sdk_directory = android_sdk_directory
 
-        #build-tools
-        if platform.system() == 'Darwin': #맥
-            from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-macosx.zip"
-            if not os.path.exists(from_zip):
-                zip_file = zipfile.ZipFile(from_zip)
-                zip_file.extractall(self.android_sdk_directory)
-                zip_file.close()
-                shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
-        elif platform.system() == 'Windows': #윈도우
-            from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-windows.zip"
-            if not os.path.exists(from_zip):
-                zip_file = zipfile.ZipFile(from_zip)
-                zip_file.extractall(self.android_sdk_directory)
-                zip_file.close()
-                shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
-        elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
-            from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-linux.zip"
-            if not os.path.exists(from_zip):
-                zip_file = zipfile.ZipFile(from_zip)
-                zip_file.extractall(self.android_sdk_directory)
-                zip_file.close()
-                shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
+        if self.android_sdk_directory:
+            #build-tools
+            build_tools_directory = f"{self.android_sdk_directory}/build-tools"
+            if not os.path.exists(build_tools_directory):
+                if platform.system() == 'Darwin': #맥
+                    from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-macosx.zip"
+                    zip_file = zipfile.ZipFile(from_zip)
+                    zip_file.extractall(self.android_sdk_directory)
+                    zip_file.close()
+                    shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
+                elif platform.system() == 'Windows': #윈도우
+                    from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-windows.zip"
+                    zip_file = zipfile.ZipFile(from_zip)
+                    zip_file.extractall(self.android_sdk_directory)
+                    zip_file.close()
+                    shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
+                elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
+                    from_zip = f"{self.android_sdk_directory}/build-tools_r34-rc4-linux.zip"
+                    zip_file = zipfile.ZipFile(from_zip)
+                    zip_file.extractall(self.android_sdk_directory)
+                    zip_file.close()
+                    shutil.move(f"{self.android_sdk_directory}/android-UpsideDownCake", f"{self.android_sdk_directory}/build-tools")
+    
